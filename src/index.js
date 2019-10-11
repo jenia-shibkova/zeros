0fe53dbc3cd29b02dec10acca0d81b847bca7067
Eugenia Shibkova <shibkova91@gmail.com>
2019-10-11 19:26:52 +0300

    feat: add partial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const arr = expression.split('*');
+  let zeros = 0;
+
+  for (let i = 0; i < arr.length; i++) {    
+    let number = +arr[i].match(/\d+/);   
+
+    if (arr[i].match(/\!{2}/)) {           // if factorial!!  
+      if (number % 2 === 0) {                       // чётные 
+        zeros += Math.floor(number / 10); 
+      } else if (number % 2 !== 0) {                // нечётные
+        zeros += Math.floor(number / 5); 
+      }
+    } else {                              // else factorial!
+      while (number) {
+        number = (number / 5) | 0 ;
+        zeros += number;
+      }                             
+      
+    }
+  }     
+
+  return zeros;
 }
+
+
+
